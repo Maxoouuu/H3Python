741e0ad38ced0be96c3ecf872db314e078d5397b
Maxime Coriton <76161577+Maxoouuu@users.noreply.github.com>
2022-09-13 12:18:47 +0200

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-tab = []
-a = int(input("Combien de valeurs ? "))
-for i in range(a):
-  num = int(input("Entrez votre nombre : "))
-  tab.append(num)
-listSum = sum(tab)
-print("la somme des valeurs de la liste est : " , listSum)
